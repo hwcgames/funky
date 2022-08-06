2524c8f610c6816c11c0360be9b7b77ae5279e07
Willow <willow.ch@protonmail.com>
2022-08-06 15:16:56 -0500

    Fixed builds

--- build.sh
+++ build.sh
@@ -5,19 +5,27 @@ fi
 which godot > /dev/null && export godot="godot" || export godot="flatpak run org.godotengine.Godot"
 
 # Create destination
-export BUILD_DIR=./build/$FUNKY_TARGET
-mkdir -p $BUILD_DIR
 # Determine godot output profile
 if [ "$FUNKY_TARGET" == "Darwin" ]; then
 	export GODOT_PROFILE="Mac OSX"
 	export EXTENSION="app"
+	export BUILD_DIR=./build/mac
 elif [ "$FUNKY_TARGET" == "Windows" ]; then
 	export GODOT_PROFILE="Windows Desktop"
 	export EXTENSION="exe"
+	export BUILD_DIR=./build/windows
 else
 	export GODOT_PROFILE="Linux/X11"
 	export EXTENSION="run"
+	export BUILD_DIR=./build/linux
 fi
+mkdir -p $BUILD_DIR
 # Run godot build
-$godot --export "$GODOT_PROFILE" $BUILD_DIR/FUNKY.$EXTENSION
+$godot --no-window --export "$GODOT_PROFILE" $BUILD_DIR/FUNKY.$EXTENSION
+# Copy license and attribution
+cp LICENSE ATTRIBUTION $BUILD_DIR
+# Copy songs
+cp -r songs $BUILD_DIR
+# Clean already-imported files from songs dir
+rm -r $BUILD_DIR/songs/**/*.{import,ogg}
 echo "DONE!"
